3745f1f66f07b04ee98e184ab276f86fb99e29d1
walid hamdi <walidhamdidev@gmail.com>
2023-02-04 22:26:56 +0100

    create listview card of newsletter

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import '../../widgets/custom_container.dart';
 import '../../widgets/scaffold_wrapper.dart';
-import 'widgets/article_list.dart';
 import 'widgets/label.dart';
 import 'widgets/navbar.dart';
 import 'widgets/newsletter_list.dart';
@@ -15,27 +14,23 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScaffoldWrapper(
       child: CustomContainer(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: const [
-              Navbar(),
-              SizedBox(height: 20),
-              SearchBox(),
-              SizedBox(height: 10),
-              Label(
-                label: 'Newsletter',
-              ),
-              NewsletterList(),
-              Label(
-                label: 'Article',
-              ),
-              SizedBox(height: 10),
-              ArticleList(),
-              NewsletterList(),
-            ],
-          ),
+        child: ListView(
+          children: const [
+            Navbar(),
+            SizedBox(height: 20),
+            SearchBox(),
+            Label(
+              label: 'Newsletter',
+            ),
+
+            NewsletterList(),
+
+            // Label(
+            //   label: 'Article',
+            // ),
+            // ArticleList(),
+            // NewsletterList(),
+          ],
         ),
       ),
     );
--- lib/views/home/widgets/newsletter_card.dart
+++ lib/views/home/widgets/newsletter_card.dart
@@ -9,74 +9,125 @@ class NewsletterCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width / 1.2,
-      decoration: BoxDecoration(
-        color: Colors.white,
+    return Card(
+      color: Colors.white,
+      shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10),
-        boxShadow: const [
-          BoxShadow(
-            color: Colors.black12,
-            offset: Offset(0, 2),
-            blurRadius: 6,
+      ),
+      elevation: 6,
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(10),
+              topRight: Radius.circular(10),
+            ),
+            child: Image.network(
+              newsletter.imageUrl,
+              height: 90,
+              width: double.infinity,
+              fit: BoxFit.fitWidth,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              margin: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    newsletter.title,
+                    style: const TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    "${newsletter.description.substring(0, 40)}...",
+                    // style: const TextStyle(overflow: TextOverflow.visible),
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 10, vertical: 5),
+                        decoration: BoxDecoration(
+                          color: Colors.blue[200],
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Text(
+                          newsletter.readTime,
+                          style: const TextStyle(
+                            fontSize: 12,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 10, vertical: 5),
+                        decoration: BoxDecoration(
+                          color: Colors.blue[200],
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Text(
+                          newsletter.writer,
+                          style: const TextStyle(
+                            fontSize: 12,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Expanded(
+                    child: Row(
+                      children: [
+                        const Icon(Icons.thumb_up),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        const Icon(Icons.bookmark_border),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        const Icon(Icons.share),
+                        const SizedBox(
+                          width: 20,
+                        ),
+                        Flexible(
+                          child: Container(
+                            // alignment: Alignment.topLeft,
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 12,
+                              vertical: 8,
+                            ),
+                            decoration: BoxDecoration(
+                              color: Colors.red[200],
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Text(
+                              newsletter.topic,
+                              style: const TextStyle(
+                                // fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
-      ),
-      child: Expanded(
-        child: Column(
-          children: <Widget>[
-            ClipRRect(
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(10),
-                topRight: Radius.circular(10),
-              ),
-              child: Image.network(
-                newsletter.imageUrl,
-                height: 40,
-                width: double.infinity,
-                fit: BoxFit.cover,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(15),
-              child: Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      newsletter.title,
-                      style: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    const SizedBox(height: 10),
-                    Text(
-                        "${newsletter.description.substring(0, 30)}... read more"),
-                    const SizedBox(height: 10),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text("Read Time: ${newsletter.readTime}"),
-                        Text("Writer: ${newsletter.writer}"),
-                        Text("Topic: ${newsletter.topic}"),
-                      ],
-                    ),
-                    const SizedBox(height: 10),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: const [
-                        Icon(Icons.thumb_up),
-                        Icon(Icons.bookmark_border),
-                        Icon(Icons.share),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
       ),
     );
   }
