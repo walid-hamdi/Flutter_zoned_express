00145cb200ad60381d458cd13fae9b3b01e77612
walid hamdi <walidhamdidev@gmail.com>
2023-05-16 12:28:57 +0100

    update env variable to hide firebase info keys

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import './widgets/my_app.dart';
 
+
 void main() async {
+  await dotenv.load(fileName: ".env");
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
